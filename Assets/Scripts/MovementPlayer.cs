c8c92cd0493ed29b916031c69f83010dcfb6eae4
Gergely Farago <farcsi379@gmail.com>
2019-09-19 19:17:32 +0200

    First commit. Make this game awsome together!

---
+++ Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    public CharacterController2D characterController2D;
+    public float runSpeed = 40f;
+    float horizontalMove = 0;
+    bool jump = false;
+    bool crouch = false;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        characterController2D.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}

4f15a94230fd7e5fa3bccc475544f308ab3b13ea
Gergely Farago <farcsi379@gmail.com>
2019-09-26 09:14:12 +0200

    Added jump button and animations. Fixed moving.

--- Assets/Scripts/MovementPlayer.cs
+++ Assets/Scripts/MovementPlayer.cs
@@ -2,25 +2,49 @@
 
 public class MovementPlayer : MonoBehaviour
 {
+    public Joystick joystick;
+    public FixedJoystickButton joystickButton;
+    public Animator animator;
     public CharacterController2D characterController2D;
-    public float runSpeed = 40f;
-    float horizontalMove = 0;
+    
+    public float runSpeed = 30f;
+    float horizontalMove = 0f;
     bool jump = false;
-    bool crouch = false;
 
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        horizontalMove = joystick.Horizontal * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-        if (Input.GetButtonDown("Jump"))
+        if(joystick.Horizontal >= .2f)
+        {
+            horizontalMove = runSpeed;
+        }else if(joystick.Horizontal <= -.2f)
+        {
+            horizontalMove = -runSpeed;
+        }
+        else
+        {
+            horizontalMove = 0;
+        }
+
+        float verticalMove = joystick.Vertical;
+
+        if(!jump && joystickButton.pressed)
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     void FixedUpdate()
     {
-        characterController2D.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        characterController2D.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
         jump = false;
     }
 }
